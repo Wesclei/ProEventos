905ed3fe0d3d9df5d447e6259033ec55a6e27147
Wesclei Araujo <wescleiaraujo@gmail.com>
2021-04-23 18:45:14 -0300

    EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,63 +4,45 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class EventoController : ControllerBase
-  {
-    public IEnumerable<Evento> _evento = new Evento[]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
     {
-        new Evento(){
-          EventoId = 1,
-          Tema = "Angular 11 e .NET 5",
-          Local = "São Paulo - SP",
-          QtdPessoas = 250,
-          Lote = "1° Lote",
-          DataEvento = DateTime.Now.AddDays(2).ToString(),
-          ImagemURL = "foto1.png"
-        },
-        new Evento(){
-          EventoId = 2,
-          Tema = "Angular 11",
-          Local = "São Paulo - SP",
-          QtdPessoas = 300,
-          Lote = "1° Lote",
-          DataEvento = DateTime.Now.AddDays(2).ToString(),
-          ImagemURL = "foto2.png"
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
+        {
+            _context = context;
         }
-    };
-   public EventoController(){
-    }
 
-    
-    [HttpGet]
-    public IEnumerable<Evento> Get()
-    {
-      return _evento;
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _context.Eventos;
+        }
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+        }
+        [HttpPost]
+        public string Post()
+        {
+            return "EX Post";
+        }
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"EX Put com id = {id}";
+        }
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"EX Delete com id = {id}";
+        }
     }
-    [HttpGet("{id}")]
-    public IEnumerable<Evento> GetById(int id)
-    {
-      return _evento.Where(evento => evento.EventoId == id);
-    }
-    [HttpPost]
-      public string Post()
-    {
-        return "EX Post";
-    }
-    [HttpPut("{id}")]
-      public string Put(int id)
-    {
-        return $"EX Put com id = {id}";
-    }
-    [HttpDelete("{id}")]
-      public string Delete(int id)
-    {
-        return $"EX Delete com id = {id}";
-    }
-  }
 }
